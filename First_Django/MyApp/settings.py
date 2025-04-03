79e543364a6ea730bf5e2bfcb0e8d5896d36f081
Praveen sundar <131776093+spraveensundar@users.noreply.github.com>
2025-04-03 13:23:27 +0530

    Update settings.py

--- First_Django/MyApp/settings.py
+++ First_Django/MyApp/settings.py
@@ -84,11 +84,11 @@ WSGI_APPLICATION = 'MyApp.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'EmployeeDB',
-        'USER': 'postgres',
-        'PASSWORD': 'iamgroot@123',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'NAME': 'YOUR_DB_NAME',
+        'USER': 'USER_NAME',
+        'PASSWORD': 'DB_PASSWORD',
+        'HOST': 'HOST',
+        'PORT': 'PORT',
     }
 }
 
